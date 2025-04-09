fe1ec818040855c6587cb0d27413c47c1364526c
Shrikant Vishwakarma <shrikant@zkcross.network>
2025-04-09 13:20:18 +0530

    remove range selection

--- src/components/dashboard/GraphSection.tsx
+++ src/components/dashboard/GraphSection.tsx
@@ -4,7 +4,7 @@
 import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import VolumeChart from "../VolumeChart";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { baseUrl } from "@/service/api";
 
 interface GraphSectionProps {
@@ -16,7 +16,7 @@ interface GraphSectionProps {
 }
 
 const GraphSection = ({ label, apiEndpoint, dataMapping, hoverLabel, statsDate }: GraphSectionProps) => {
-    const [timeframe, setTimeframe] = useState("all");
+    const [timeframe] = useState("all"); //setTimeframe
     const { data, isPending, error } = useQuery({
         queryKey: [label, timeframe],
         queryFn: () => fetch(`${baseUrl}${apiEndpoint}?fromTimeframe=${timeframe}`).then((res) => res.json()),
@@ -31,7 +31,7 @@ const GraphSection = ({ label, apiEndpoint, dataMapping, hoverLabel, statsDate }
         <div>
             <div className="flex justify-between items-center mb-2">
                 <h3 className="text-lg font-semibold">{label}</h3>
-                <Select onValueChange={(value) => setTimeframe(value)} defaultValue={timeframe}>
+                {/* <Select onValueChange={(value) => setTimeframe(value)} defaultValue={timeframe}>
                     <SelectTrigger className="w-32">
                         <SelectValue placeholder="Select Timeframe" />
                     </SelectTrigger>
@@ -41,7 +41,7 @@ const GraphSection = ({ label, apiEndpoint, dataMapping, hoverLabel, statsDate }
                         <SelectItem value="1y">1 Year</SelectItem>
                         <SelectItem value="all">All</SelectItem>
                     </SelectContent>
-                </Select>
+                </Select> */}
             </div>
             <VolumeChart isPending={isPending} label={label} data={chartData} date={statsDate || ""} hoverLabel={hoverLabel} />
         </div>
